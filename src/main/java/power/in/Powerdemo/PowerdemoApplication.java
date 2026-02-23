81113625dc9aa79fa19933e600134ec601705778
Ratz <rathnapandian7@gmail.com>
2026-02-22 23:58:56 -0500

    first commit

---
+++ src/main/java/power/in/Powerdemo/PowerdemoApplication.java
@@ -0,0 +1,13 @@
+package power.in.Powerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PowerdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PowerdemoApplication.class, args);
+	}
+
+}
